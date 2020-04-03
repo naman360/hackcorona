750b13bf604361f2ecaadc767a3f9a55b0b7aab4
Naman Shankhydhar <naman.geek@gmail.com>
2020-04-03 20:41:38 +0530

    Changes restored

--- js/bot.js
+++ js/bot.js
@@ -323,7 +323,6 @@ return botui.action.button({
 }).then(() =>{
   document.getElementById('result').style.display = 'block';
   document.getElementById('help-bot').style.display = 'none';
-  document.getElementById('phone').style.display = 'none';
   let perc = score*100/80;
   let msg='';
 
